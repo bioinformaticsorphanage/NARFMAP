f1c7e270d297180a67141ae29626f654f657d4fd
Edmund Miller <git@edmundmiller.dev>
2024-06-27 11:37:57 -0500

    build: Create bindings on the fly

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
 use std::process::Command;
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
 
 fn main() -> miette::Result<()> {
     println!("cargo:rerun-if-changed=Makefile");
@@ -8,6 +12,15 @@ fn main() -> miette::Result<()> {
         .expect("failed to run \"make clean\"");
     assert!(status.success());
 
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
     // build dragen-os command and dragen static library
     // let status = Command::new("make")
     //     .status()
---
+++ wrapper.h
@@ -0,0 +1,8 @@
+#include "thirdparty/dragen/src/common/hash_generation/crc_hash.h"
+#include "thirdparty/dragen/src/common/hash_generation/gen_hash_table.h"
+#include "thirdparty/dragen/src/common/hash_generation/hash_cfg_file.h"
+#include "thirdparty/dragen/src/common/hash_generation/hash_table.h"
+#include "thirdparty/dragen/src/common/hash_generation/hash_table_compress.h"
+#include "thirdparty/dragen/src/common/hash_generation/host_version_hashtable.h"
+#include "thirdparty/dragen/src/common/hash_generation/liftover.h"
+#include "thirdparty/dragen/src/common/hash_generation/methylation_hash_table.h"
